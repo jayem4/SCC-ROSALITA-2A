8493fdffb6aacf7239f6b991c04c5a8fc483c357
jayem4 <jayemrosalita23@gmail.com>
2024-09-20 12:29:48 +0800

    apdeyt

--- src/rosalita/Account.java
+++ src/rosalita/Account.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package rosalita;
+package javaapplicationnew;
 
-/**
- *
- * @author SCC-COLLEGE
- */
+
+
 public class Account {
-    
+      
+    int lastId = 0;
+    int id;
+  String firstName;
+  String lastName;
+  String email;
+  String username;
+  String password;
+
+    public Account(String firstName, String lastName, String email, String username, String password) {
+        this.id = ++lastId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String toString() {
+        System.out.println("-----------------------------------------------------------------------------");
+        return "ID: " + id + " | First Name: " + firstName + "| Last Name: " + lastName + 
+               "| Email: " + email + "| Username: " + username;
+    }
 }
